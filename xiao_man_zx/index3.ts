63f0a52874d56602eb8e1b62c4272500ee20248d
Rstars-Even <473753137@qq.com>
2023-08-28 18:38:38 +0800

    基本类型学习4。

--- xiao_man_zx/index3.ts
+++ xiao_man_zx/index3.ts
@@ -57,3 +57,43 @@ function xiao(val: xiao) {
     }
 }
 // console.log(xiao('324'));
+
+
+function xiaoman<T>(a:T, b:T):Array<T> {
+    return [a, b]
+}
+xiaoman(1, 2)
+xiaoman('1', '2')               //动态类型。
+xiaoman(false, true)            //
+
+
+function add<T = number, K = number>(a:T, b:K):Array<T | K> {       //给定默认类型。
+    return [a, b]
+}
+add(false, 1)
+add(false, '1')
+
+function prop<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key]
+}
+let o = {a: 1, b: 2, c: 3}
+prop(o, 'a')
+// prop(o, 'd')
+
+//在类型后面跟一个 extends 再跟一个约束的类型
+function add2<T extends number>(a:T, b:T) {
+    return a + b
+}
+add2(1, 2)
+// add2('j', "6")
+
+interface Len {
+    length: number
+}
+function add3<T extends Len>(a: T) {
+    a.length
+}
+add3('11111111')
+add3([1, 2, 3])
+// add3(false)
+// add3(11111111)
