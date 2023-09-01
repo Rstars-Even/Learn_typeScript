e1fcca32451c21dd6eef652b3ad91804c3e783d5
Rstars-Even <473753137@qq.com>
2023-09-01 18:39:27 +0800

    基本类型学习5。

--- xiao_man_zx/index3.ts
+++ xiao_man_zx/index3.ts
@@ -21,79 +21,144 @@ import { type } from "os"
 // console.log(Types.Green);       //1
 // console.log(Types.Blue);        //2
 
-enum Color {
-    names = '张三'
-}
-interface AA {
-    names: Color.names
-}
-let objs: AA = { names: Color.names }
+// enum Color {
+//     names = '张三'
+// }
+// interface AA {
+//     names: Color.names
+// }
+// let objs: AA = { names: Color.names }
 
-let s = "张三"
-s = "345"
+// let s = "张三"
+// s = "345"
 
-type a = 1 extends any ? 1 : 0 //1
+// type a = 1 extends any ? 1 : 0 //1
 
-type a1 = 1 extends never ? 1 : 0 //0
+// type a1 = 1 extends never ? 1 : 0 //0
 
-type a2 = 1 extends number ? 1 : 0 //1
+// type a2 = 1 extends number ? 1 : 0 //1
 
-type value = boolean | 0 | '213'
-let ss: value = 0
+// type value = boolean | 0 | '213'
+// let ss: value = 0
 
-type xiao = '小满' | '中满' | '大满'
+// type xiao = '小满' | '中满' | '大满'
 
-function xiao(val: xiao) {
-    switch (val) {
-        case '小满':
-            break
-        case '中满':
-            break
-        case '大满':
-            break
-        default:
-            const error: never = val
-            return error
-    }
-}
-// console.log(xiao('324'));
-
-
-function xiaoman<T>(a:T, b:T):Array<T> {
-    return [a, b]
-}
-xiaoman(1, 2)
-xiaoman('1', '2')               //动态类型。
-xiaoman(false, true)            //
+// function xiao(val: xiao) {
+//     switch (val) {
+//         case '小满':
+//             break
+//         case '中满':
+//             break
+//         case '大满':
+//             break
+//         default:
+//             const error: never = val
+//             return error
+//     }
+// }
+// // console.log(xiao('324'));
 
 
-function add<T = number, K = number>(a:T, b:K):Array<T | K> {       //给定默认类型。
-    return [a, b]
-}
-add(false, 1)
-add(false, '1')
+// function xiaoman<T>(a:T, b:T):Array<T> {
+//     return [a, b]
+// }
+// xiaoman(1, 2)
+// xiaoman('1', '2')               //动态类型。
+// xiaoman(false, true)            //
 
-function prop<T, K extends keyof T>(obj: T, key: K) {
-    return obj[key]
-}
-let o = {a: 1, b: 2, c: 3}
-prop(o, 'a')
-// prop(o, 'd')
 
-//在类型后面跟一个 extends 再跟一个约束的类型
-function add2<T extends number>(a:T, b:T) {
-    return a + b
-}
-add2(1, 2)
-// add2('j', "6")
+// function add<T = number, K = number>(a:T, b:K):Array<T | K> {       //给定默认类型。
+//     return [a, b]
+// }
+// add(false, 1)
+// add(false, '1')
 
-interface Len {
-    length: number
-}
-function add3<T extends Len>(a: T) {
-    a.length
-}
-add3('11111111')
-add3([1, 2, 3])
+// function prop<T, K extends keyof T>(obj: T, key: K) {
+//     return obj[key]
+// }
+// let o = {a: 1, b: 2, c: 3}
+// prop(o, 'a')
+// // prop(o, 'd')
+
+// //在类型后面跟一个 extends 再跟一个约束的类型
+// function add2<T extends number>(a:T, b:T) {
+//     return a + b
+// }
+// add2(1, 2)
+// // add2('j', "6")
+
+// interface Len {
+//     length: number
+// }
+// function add3<T extends Len>(a: T) {
+//     a.length
+// }
+// add3('11111111')
+// add3([1, 2, 3])
 // add3(false)
 // add3(11111111)
+
+namespace a {
+    export const Time: number = 1000
+    // export const fn = <T>(arg: T): T => {
+    //     return arg
+    // }
+    // fn(Time)
+}
+
+
+namespace a {
+    export const Times: number = 1000
+    // export const fn = <T>(arg: T): T => {
+    //     return arg
+    // }
+    // fn(Time)
+}
+
+console.log(a.Times);
+// console.log(b.Times);
+
+interface Name {
+    name: string
+}
+interface Age {
+    age: number
+}
+interface Sex {
+    sex: number
+}
+
+let people1: Name = { name: "小满" }
+let people2: Age = { age: 20 }
+let people3: Sex = { sex: 1 }
+
+const people = Object.assign(people1, people2, people3)
+
+
+// class A {
+//     type: boolean = false;
+//     changeType() {
+//         this.type = !this.type
+//     }
+// }
+// class B {
+//     name: string = '张三';
+//     getName(): string {
+//         return this.name;
+//     }
+// }
+
+// class C implements A, B {
+//     type: boolean
+//     changeType: () => void;
+//     name: string;
+//     getName: () => string
+// }
+// Mixins(C, [A, B])
+// function Mixins(curCls: any, itemCls: any[]) {
+//     itemCls.forEach(item => {
+//         Object.getOwnPropertyNames(item.prototype).forEach(name => {
+//             curCls.prototype[name] = item.prototype[name]
+//         })
+//     })
+// }
